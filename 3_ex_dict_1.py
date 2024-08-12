ef764ed875e8cf79b4782618b0b659a452f0b930
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-08-12 11:02:23 -0300

    Add files via upload

---
+++ 3_ex_dict_1.py
@@ -0,0 +1,10 @@
+'''Crie um dicionário que guarde as seguintes informações:
+a) Um time de futebol identificado pela chave time.
+b) Um texto qualquer identificado pela chave texto.'''
+
+dicionario = {
+    'Time' : 'São Paulo',
+    'Texto' : 'Um tetxo qualquer'
+}
+
+print(dicionario)
