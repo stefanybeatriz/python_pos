b14e12228a3130662be2f2321d2ec4e4cdfb002e
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-08-12 11:00:14 -0300

    Add files via upload

---
+++ 3_ex_aritmetica_1.py
@@ -0,0 +1,5 @@
+# Crie uma variável resultado que receba o valor da operação 3 x (4 + 7) e imprima seu resultado.
+
+resultado = 3 * (4 + 7)
+
+print(f'O resultado da operação: 3 * (4 + 7) é: {resultado}')
