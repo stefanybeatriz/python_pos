5247a864e3b59340c5220531e7c88fc2bd104198
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-08-12 11:02:48 -0300

    Add files via upload

---
+++ 3_ex_dict_2.py
@@ -0,0 +1,9 @@
+'''Imprima os valores de seu dicionário criado no exercício 1 seguindo o padrão:
+'O time que escolhi foi o _________ e meu texto é ___________.'''
+
+dicionario = {
+    'Time' : 'São Paulo',
+    'Texto' : 'Um tetxo qualquer'
+}
+
+print(f'O time que escolhi foi o {dicionario["Time"]} e o meu texto é {dicionario["Texto"]}')
