f42101e1f1a7ecd8574d73a541ee580a5cae1a84
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-08-12 11:01:33 -0300

    Add files via upload

---
+++ 3_ex_condicao_1.py
@@ -0,0 +1,10 @@
+'''Faça uma condição que compara a variável numero e imprime Número maior ou igual a 30 caso:
+- o número seja maior ou igual a 30
+- número menor que 30 caso o número seja menor que 30.'''
+
+numero = int(input('Digite um valor: '))
+
+if numero >= 30:
+    print('Este número é igual maior ou igual a 30')
+else:
+    print('Este número é menor que 30')
