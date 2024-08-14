018a9f795452b7b868a3659ef43b79fb9e5d7471
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-08-14 14:03:05 -0300

    Add files via upload

---
+++ 3_ex_string_2.py
@@ -0,0 +1,11 @@
+'''O código abaixo retorna um erro de execução. 
+você consegue identificar qual é este erro pela mensagem de retorno? 
+Identificado o erro, reescreva o código de maneira que ele seja executado sem erro, imprimindo a string desejada.'''
+
+# Código:
+print(Este texto deveria ser impresso na tela)
+
+# ERRO: se da pela ausência das aspas
+
+# Código Correto:
+print('Este texto deveria ser impresso na tela')
