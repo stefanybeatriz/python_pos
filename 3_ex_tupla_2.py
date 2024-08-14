8f122e8566051b74559944345de95d83cc817c99
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-08-14 14:03:54 -0300

    Add files via upload

---
+++ 3_ex_tupla_2.py
@@ -0,0 +1,8 @@
+# Tente descobrir o motivo do erro abaixo.
+
+tupla = (6, 77, 42, 10)
+tupla[2] = 99
+
+'''ERROS:
+1 - O valor na posição 2 equivale a 42
+2 - O valor 42 não pode ser substituído pois a tupla é imutável'''
