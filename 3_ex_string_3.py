adce18bff00a6480cd1d2a86b5eba347df76e8a0
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-08-14 14:03:19 -0300

    Add files via upload

---
+++ 3_ex_string_3.py
@@ -0,0 +1,9 @@
+# Realize um cálculo qualquer e imprima seu resultado na tela utilizando a função format() seguindo o padrão: 
+# 'A operação _______ resulta o valor ______.'
+
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+
+soma = valor1 + valor2
+
+print(f'A operação {valor1} + {valor2} resulta o valor {soma}')
