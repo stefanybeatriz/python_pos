b0abdd78a950d4eb9587b2070a332ff4b264caf8
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2025-03-17 17:02:06 -0300

    Add files via upload

---
+++ 3_ex_def_1.py
@@ -0,0 +1,9 @@
+# Crie uma função que receba como parâmetro a lista valores abaixo e retorne a soma de todos os valores da lista.
+
+valores = [55, 92, 427, 86, 10, 64, 89, 177]
+
+def soma_da_lista(valores):
+    print(f'A soma de todos os valores da lista equivale a: {sum(valores)}')
+
+# Teste
+soma_da_lista(valores)
