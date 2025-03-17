e92a04974d15090eed6bc4c58af3ffd6b3ac4036
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2025-03-17 17:03:25 -0300

    Add files via upload

---
+++ 3_ex_loop_1.py
@@ -0,0 +1,6 @@
+# Percorra a lista abaixo utilizando um for loop e imprima o valor de cada índice.
+
+filmes = ['Filme 1', 'Filme 2', 'Filme 3', 'Filme 4']
+
+for index, item in enumerate(filmes):
+    print('índice: ', index)
